d9adebbcb338ee415c4c4edc24185c87c85cfca8
Maks <makson94021@gmail.com>
2022-12-24 21:15:46 +0200

    complete receiver, done refactoring

--- bot.js
+++ bot.js
@@ -1,36 +1,106 @@
+const roflArray = require('./assets/rofls');
+const answerForUsers = require('./assets/answerForUsersKtoI');
+
+require('dotenv').config();
+const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = '5735930962:AAFjGUCmSoiorJdnaXv0Thg4QwquFw9g8pE';
-    
+
+const DATABASE_PATH = './database/receiveMessages.json';
+const TOKEN = process.env.TOKEN;
+
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-const messagesAfterPin = [
-    'Ех, опять дрочить',
-    'Спасибо принцесе Виктории 😊',
-    'Люблю Серегу ❤️',
-    'Люблю физика ❤️',
-    'Люблю писать код на пхп 😍',
-    `Эй, цепь на мне, сыпь лавэ
-Сотка тыщ на bag LV
-Сотни сук хотят ко мне
-Сотни сук хотят камней
-Как дела? Как дела?
-Это новый Cadillac
-Делать деньги, делать деньги
-Делать деньги, блять, вот так (окей, few)`,
-    `Делаю вдох, так пахнет Dior
-Я искал тебя вечность — вот идиот
-Делаю вдох, так пахнет Dior
-Я искал тебя вечность — вот идиот`
-];
 
-bot.onText(/Уроки на/, async function(msg) {
-    const chatId = msg.chat.id;
-    const messageId = msg.message_id;
+bot.onText(/\/rofl/, msg => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
 
-    const randomNumber = Math.floor(Math.random() * messagesAfterPin.length);
-    const randomMessageAfterPin = messagesAfterPin[randomNumber];
+  const randomNumber = Math.floor(Math.random() * roflArray.length);
+  const randomRofl = roflArray[randomNumber];
 
-    await bot.unpinChatMessage(chatId);
-    await bot.pinChatMessage(chatId, messageId);
-    await bot.sendMessage(chatId, randomMessageAfterPin);
+  bot.deleteMessage(chatId, messageId);
+  bot.sendMessage(chatId, randomRofl);
+});
+
+bot.onText(/\/ne_umnichai/, msg => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
+
+  const messageForUmniki = `Нет ничего утомительнее, чем присутствовать при том, как человек демонстрирует свой ум. В особенности, если ума нет.
+(С) Эрих Мария Ремарк`;
+
+  bot.deleteMessage(chatId, messageId);
+  bot.sendMessage(chatId, messageForUmniki);
+});
+
+bot.onText(/\/kto_i/, msg => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
+  const senderUserName = msg.from.username;
+
+  const messageForUser = answerForUsers[senderUserName];
+
+  bot.deleteMessage(chatId, messageId);
+  bot.sendMessage(
+    chatId,
+    messageForUser !== undefined ? messageForUser : answerForUsers['unknown']
+  );
+});
+
+bot.on('document', msg => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Ммммм, домашечка, спасибо тебе 😘');
+});
+
+bot.onText(/\/receive (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const senderUserName = msg.from.username;
+  const messageDate = new Date().toLocaleString(msg.date);
+  const messageId = msg.message_id;
+  const receiveMessage = match[1];
+  const chatType = msg.chat.type;
+
+  const message = {
+    sender: senderUserName,
+    chatType: chatType,
+    date: messageDate,
+    chat: chatId,
+    text: receiveMessage
+  };
+
+  const oldMessages =
+    fs.readFileSync(DATABASE_PATH, 'utf-8') || '[]';
+
+  const messages = JSON.parse(oldMessages);
+  messages.push(message);
+
+  fs.writeFileSync(DATABASE_PATH, JSON.stringify(messages));
+
+  bot.deleteMessage(chatId, messageId);
+  bot.sendMessage(chatId, 'Сообщение успешно сохранено!');
+});
+
+bot.onText(/\/show_all_received_message/, msg => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
+
+  const data = fs.readFileSync(DATABASE_PATH, 'utf-8');
+  const messages = JSON.parse(data);
+
+  let sendingText = '';
+  sendingText = sendingText.concat('--------------------------------');
+  messages.map(message => {
+    sendingText = sendingText.concat(`--------------------------------
+-   Отправитель: ${message.sender}
+-   ID чата: ${message.chat}
+-   Тип чата: ${message.chatType}
+-   Текст сообщения: ${message.text}
+-   Дата сохранения: ${message.date}
+--------------------------------`);
+  });
+  sendingText = sendingText.concat('--------------------------------');
+
+  bot.deleteMessage(chatId, messageId);
+  bot.sendMessage(chatId, sendingText);
 });
